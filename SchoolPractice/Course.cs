570f0c0d34ba478dca24d6d5423dd96003c6cae2
jomsiess <jomsiess@gmail.com>
2020-11-16 16:30:18 -0600

    Objects 1 excercise

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolPractice
+{
+    public class Course
+    {
+        public List<Student> Students {get; set;}
+        public string CourseName { get; set; }
+        public int Credits { get; set; }
+
+        public Course(List<Student>students, string courseName, int credits)
+        {
+            Students = students;
+            CourseName = courseName;
+            Credits = credits; 
+        }
+    }
+}
